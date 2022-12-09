ff0581a60a796b2ebc058a58c2c26c81dcfea377
sean-murray <sean.murray2000@hotmail.com>
2022-12-09 17:15:09 -0500

    fix LVN

--- highlevel_optimizations.cpp
+++ highlevel_optimizations.cpp
@@ -315,10 +315,13 @@ void LocalValueNumbering::invalidate_mappings(Operand op) {
 }
 
 void LocalValueNumbering::fix_commutativity(std::vector<Operand> &right_side) {
+  printf("Fixing...\n");
   bool swap = false;
   if (right_side[0].has_base_reg() && right_side[1].has_base_reg())
-    if (right_side[1] < right_side[0]) 
+    if (right_side[1] < right_side[0]) {
       swap = true;
+      printf("both reg and right < left...\n");
+    }
   else if (right_side[1].has_base_reg())
     swap = true;
   if (swap) {
