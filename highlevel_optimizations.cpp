62adb512cbd17d1cf7a745a6f56155afec46646e
sean-murray <sean.murray2000@hotmail.com>
2022-12-10 17:41:53 -0500

    finish optimizations

--- highlevel_optimizations.cpp
+++ highlevel_optimizations.cpp
@@ -36,13 +36,13 @@ std::shared_ptr<InstructionSequence> HighLevelOptimizer::optimize(std::shared_pt
     //cfg = constant_prop.transform_cfg();
     // LVN
     LocalValueNumbering lvn(cfg);
-    //cfg = lvn.transform_cfg();
+    cfg = lvn.transform_cfg();
     // Copy propagation
     CopyPropagation copy_prop(cfg);
     //cfg = copy_prop.transform_cfg();
     // Dead store elimination
     DeadStoreElimination dead_elim(cfg);
-    cfg = dead_elim.transform_cfg();
+    //cfg = dead_elim.transform_cfg();
   }
 
   // Local register allocation
