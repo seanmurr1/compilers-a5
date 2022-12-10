df50d9aea9d30964a28a73d0d8684290c25fae2e
sean-murray <sean.murray2000@hotmail.com>
2022-12-10 17:08:15 -0500

    finish optimizations

--- highlevel_optimizations.cpp
+++ highlevel_optimizations.cpp
@@ -23,7 +23,7 @@ std::shared_ptr<InstructionSequence> HighLevelOptimizer::optimize(std::shared_pt
 
   // Global callee-saved register assignment
   GlobalCalleeSavedRegAssignment global_assigner;
-  cur_hl_iseq = global_assigner.optimize(cur_hl_iseq);
+  //cur_hl_iseq = global_assigner.optimize(cur_hl_iseq);
 
   // Build CFG
   HighLevelControlFlowGraphBuilder hl_cfg_builder(cur_hl_iseq);
@@ -36,20 +36,20 @@ std::shared_ptr<InstructionSequence> HighLevelOptimizer::optimize(std::shared_pt
     cfg = constant_prop.transform_cfg();
     // LVN
     LocalValueNumbering lvn(cfg);
-    cfg = lvn.transform_cfg();
+    //cfg = lvn.transform_cfg();
     // Copy propagation
     CopyPropagation copy_prop(cfg);
-    cfg = copy_prop.transform_cfg();
+    //cfg = copy_prop.transform_cfg();
     // Dead store elimination
     DeadStoreElimination dead_elim(cfg);
-    cfg = dead_elim.transform_cfg();
+    //cfg = dead_elim.transform_cfg();
   }
 
   // Local register allocation
   LocalRegisterAllocation local_assigner(cfg);
-  cfg = local_assigner.transform_cfg();
-  int num_reg_spilled = local_assigner.get_num_reg_spilled();
-  funcdef_ast->set_max_temp_vreg(num_reg_spilled + 9);
+  //cfg = local_assigner.transform_cfg();
+  //int num_reg_spilled = local_assigner.get_num_reg_spilled();
+  //funcdef_ast->set_max_temp_vreg(num_reg_spilled + 9);
 
   // Convert transformed high-level CFG back into iseq
   cur_hl_iseq = cfg->create_instruction_sequence();
