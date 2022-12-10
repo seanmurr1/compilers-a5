086db63911a2d6ce8ea679d27166902c505a1bef
sean-murray <sean.murray2000@hotmail.com>
2022-12-10 17:26:43 -0500

    finish optimizations

--- context.cpp
+++ context.cpp
@@ -187,11 +187,11 @@ void Context::highlevel_codegen(ModuleCollector *module_collector) {
       // high-level InstructionSequence: this is useful in case information
       // about the function definition is needed by the low-level
       // code generator
-      //hl_iseq->set_funcdef_ast(child);
+      hl_iseq->set_funcdef_ast(child);
 
       /////////////////////////////////////
       HighLevelOptimizer opt;
-      hl_iseq = opt.optimize(hl_iseq);
+      //hl_iseq = opt.optimize(hl_iseq);
       // TODO
 
       module_collector->collect_function(fn_name, hl_iseq);
